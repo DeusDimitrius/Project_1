143bc54268c5658f97e3b4f4db522e7f5d4236e7
-Mitya- <user@MacBook-Air-User.local>
2017-07-03 22:04:39 +0300

    Test mac

---
+++ src/CType.java
@@ -0,0 +1,22 @@
+/**
+ * Created by user on 02.07.17.
+ */
+public class CType {
+    public static void main(String[] args) {
+
+        double sNumber = 12.78;
+        double thNumber = 100.00;
+
+        // int iNumber = 15;
+
+        double yNumber = thNumber+sNumber;
+
+        char myLitteleChar = 'b';
+
+        char myBigChar = Character.toUpperCase(myLitteleChar);
+
+        System.out.print ("Искомое число ");
+        System.out.println (yNumber);
+        System.out.println (myBigChar);
+    }
+}
